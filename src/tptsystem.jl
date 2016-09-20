dd85d211d04a4be20176258bbb2bfd3f199c9555
Shun Ueda <ueda@wood3.t.u-tokyo.ac.jp>
2016-09-20 13:04:54 +0900

    Simplify the difinition of TPTSystem

--- src/tptsystem.jl
+++ src/tptsystem.jl
@@ -1,19 +1,6 @@
 type TPTSystem{Tr <: RefSystem, Tp <: PertSystem}
-  m::Int # number of components
-  c::Vector{Float64} # conposition (mole fraction)
-  label::Vector{AbstractString} # labels of components
   ref::Tr # reference system
   pert::Tp # perturbing system
-end
-
-function TPTSystem(m::Int, c::Vector{Float64}, ref::RefSystem;
-                   label=AbstractString[])
-  return TPTSystem(m, c, label, ref, NoPert())
-end
-
-function TPTSystem(m::Int, c::Vector{Float64}, ref::RefSystem, pert::PertSystem;
-                   label=AbstractString[])
-  return TPTSystem(m, c, label, ref, pert)
 end
 
 function prdf(sys::TPTSystem)

e79615efc3f10ca03301b5ec93fd9f19499cd38a
Shun Ueda <hasundue@gmail.com>
2016-10-24 14:52:28 +0900

    Add binary AHS-WCA tests

--- test/tm_binary.jl
+++ test/tm_binary.jl
@@ -6,6 +6,12 @@ using DataFrames
 using Optim
 using Plots; gr()
 
+# setting up results directory
+!isdir("results") && mkdir("results")
+
+resdir = joinpath("results", "tm_binary")
+!isdir(resdir) && mkdir(resdir)
+
 # composition is fixed to 1:1
 N = 2
 c = [0.5, 0.5]
@@ -28,20 +34,19 @@ Sexp[2,2] = 1 + √(c[2]*c[2]) * (data[:S22] - 1)
 #
 # Fitting S with additive hard-sphere
 #
-
 function fopt(x::Vector{Float64})::Float64
   ρ = x[1]
   σ = x[2:3]
 
-  global ahs = TPT.AHSSystem(ρ = ρ, σ = σ, c = c)
-  global Scal = TPT.psf(ahs)
+  ahs = TPT.AHSSystem(ρ = ρ, σ = σ, c = c)
+  Sahs = TPT.psf(ahs)
 
   R = zeros(N,N)
 
   for i in 1:N, j in 1:N
     i > j && continue
     for k in 1:ndata
-      R[i,j] += abs(Sexp[i,j][k] - Scal[i,j](q[k]))
+      R[i,j] += abs(Sexp[i,j][k] - Sahs[i,j](q[k]))
     end
   end
 
@@ -61,14 +66,12 @@ res = optimize(fopt, x₀)
 
 (ρ, σ₁, σ₂) = Optim.minimizer(res)
 
-!isdir("results") && mkdir("results")
-
-resdir = joinpath("results", "tm_binary")
-!isdir(resdir) && mkdir(resdir)
+ahs = TPT.AHSSystem(ρ = ρ, σ = [σ₁, σ₂], c = c)
+Sahs = TPT.psf(ahs)
 
 for (i,j) in [(1,1), (1,2), (2,2)]
-  plot(Scal[i,j], qmin, qmax, label="calculation (AHS)")
-  plot!(q, Sexp[i,j], label="experimental")
+  plot(q, Sexp[i,j], label="exp")
+  plot!(Sahs[i,j], qmin, qmax, label="AHS")
   xlabel!("q (a.u.)")
   ylabel!("S")
   file = string("Fe-Ni_AHS_S", i, j)
@@ -76,11 +79,41 @@ for (i,j) in [(1,1), (1,2), (2,2)]
   png(path)
 end
 
+#
+# WCA
+#
+T = 1769.0
+
+ahs = TPT.AHSSystem(ρ = ρ₀::Float64, σ = σ₀::Vector, c = c::Vector)
+pp = TPT.Ashcroft([p[:rc][5], p[:rc][7]])
+nfe = TPT.NFE(ρ, c, T, zeros(2), [p[:zs][5], p[:zs][7]], pp)
+tb = TPT.WHTB(c, T, 12.0, [p[:zd][5], p[:zd][7]], [p[:rd][5], p[:rd][7]])
+nfetb = TPT.NFETB(nfe, tb)
+
+wca = TPT.WCASystem(ahs, T)
+sys = TPT.TPTSystem(wca, nfetb)
+
+Swca = TPT.psf(sys)
+σ_wca = sys.ref.trial.σ
+
+for (i,j) in [(1,1), (1,2), (2,2)]
+  plot(q, Sexp[i,j], label="exp")
+  plot!(Swca[i,j], qmin, qmax, label="WCA")
+  xlabel!("q (a.u.)")
+  ylabel!("S")
+  file = string("Fe-Ni_WCA_S", i, j)
+  path = joinpath(resdir, file)
+  png(path)
+end
 
 @testset "TM Binary" begin
   @testset "AHS" begin
     @test isapprox(ρ, 0.0113, atol=1e-4)
     @test isapprox(σ₁, 4.33, atol=1e-2)
     @test isapprox(σ₂, 4.21, atol=1e-2)
+  end
+  @testset "AHS-WCA" begin
+    @test isapprox(σ_wca[1], 4.53, atol=1e-2)
+    @test isapprox(σ_wca[2], 3.80, atol=1e-2)
   end
 end

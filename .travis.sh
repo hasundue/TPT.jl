54fc2b859c6172798fa9fbaa1373a71590d3f5e1
Shun Ueda <hasundue@gmail.com>
2016-10-30 17:38:34 +0900

    Revert "Modify .travis.sh not to use .netrc"
    This reverts commit 2307b38db830b1502b815894e5406718e79e0eac.

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -xe
 
+echo "machine github.com" >> ~/.netrc
+echo "login $GH_LOGIN"    >> ~/.netrc
+echo "password $GH_TOKEN" >> ~/.netrc
+
 git config --global user.email 'travis@travis-ci.org'
 git config --global user.name 'travis'
-git clone --quiet "https://$GH_LOGIN:$GH_TOKEN@github.com/hasundue/TPT-test.git"
+git clone --quiet "https://github.com/hasundue/TPT-test.git"
 cd TPT-test
 
 cp -a ../test/results "$TRAVIS_JOB_NUMBER"
@@ -10,3 +14,5 @@ git add "$TRAVIS_JOB_NUMBER"
 
 git commit -q -m "Automatically updated by Travis build #$TRAVIS_JOB_NUMBER"
 git push
+
+rm -f ~/.netrc

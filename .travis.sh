4c23425d907927799ded0d5c8551071fa555fa2b
Shun Ueda <hasundue@gmail.com>
2016-10-30 16:12:07 +0900

    Add a script to push results of tests to github

---
+++ .travis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+set +x
+echo "machine github.com" >> ~/.netrc
+echo "login $GH_LOGIN"    >> ~/.netrc
+echo "password $GH_TOKEN" >> ~/.netrc
+set -x
+
+git config --global user.email 'travis@travis-ci.org'
+git config --global user.name 'travis'
+git clone --quiet "https://github.com/hasundue/TPT-test.git"
+cd TPT-test
+
+cp -a ../test/results "$TRAVIS_JOB_NUMBER"
+git add "$TRAVIS_JOB_NUMBER"
+
+git commit -q -m "Automatically updated by Travis build #$TRAVIS_JOB_NUMBER"
+git push
+
+rm -f ~/.netrc

7cb4e99c2077229b35c961f0babde13a978438d5
Shun Ueda <hasundue@gmail.com>
2016-11-09 21:24:04 +0900

    Add a test for unary AHS

--- test/unit_ahs.jl
+++ test/unit_ahs.jl
@@ -6,6 +6,15 @@ using Base.Test
 M2m(M) = M / 1000 / 6.022e+23 / 9.109e-31
 
 #
+# Liquid Na
+# Ref: I. H. Umar et al.: J. Phys. F: Metal Phys., Vol. 4 (1974), 1691-1706.
+#
+ahs1 = TPT.AHSSystem(1, [6.235], [0.003564], [4.19e+4], 373.)
+
+S1 = TPT.entropy(ahs1)
+F1 = TPT.helmholtz(ahs1)
+
+#
 # NaK liquid alloy
 # Ref: I. H. Umar et al.: J. Phys. F: Metal Phys., Vol. 4 (1974), 1691-1706.
 #
@@ -22,10 +31,16 @@ T = 373.
 
 ahs = TPT.AHSSystem(N, σ, ρ, m, T)
 
-S = TPT.entropy(ahs) # divided by kB
-F = TPT.helmholtz(ahs)
+S2 = TPT.entropy(ahs) # divided by kB
+F2 = TPT.helmholtz(ahs)
 
-@testset "AHS" begin
-  @test isapprox(S, 9.44, atol=1e-2)
-  @test isapprox(F, -0.00938, atol=1e-5)
+@testset "Unit AHS" begin
+  @testset "Unary" begin
+    @test isapprox(S1, 7.25, atol=1e-2)
+    @test isapprox(F1, -0.00679, atol=1e-5)
+  end
+  @testset "Binary" begin
+    @test isapprox(S2, 9.44, atol=1e-2)
+    @test isapprox(F2, -0.00938, atol=1e-5)
+  end
 end

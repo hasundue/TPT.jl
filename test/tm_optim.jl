1a46cb87704c8aaa60fc807a431d0062fa0d56dd
Shun Ueda <hasundue@gmail.com>
2017-02-07 16:34:45 +0900

    Reset TM Optim to the version which optimizes only a according to liquid structures

--- test/tm_optim.jl
+++ test/tm_optim.jl
@@ -2,9 +2,7 @@ import TPT
 using Base.Test
 
 using DataFrames
-import Optim
-import NLopt
-import Dierckx
+using Optim
 using Plots; pyplot()
 
 println("--- TM Optim ---")
@@ -26,7 +24,7 @@ resdir = joinpath("results", "tm_optim")
 # Load experimental g(r) from csv files
 #
 g_exp = Vector{Vector{Tuple{Float64,Float64}}}(N)
-for i in 5
+for i in 1:N
     elem = p[:X][i]
     T = convert(Int, p[:T][i])
 
@@ -47,18 +45,16 @@ p[:T] = convert(DataArrays.DataArray{Float64,1}, p[:T])
 #
 ahs = Vector{TPT.AHS}(N)
 wca = Vector{TPT.LWCA}(N)
-# tb = Vector{TPT.WHTB}(N)
+tb = Vector{TPT.WHTB}(N)
 
-for i in 5
-  ahs[i] = TPT.AHS(σ = p[:σ][i], ρ = p[:ρ][i], approx=:RFA)
+for i in 1:N
+  ahs[i] = TPT.AHS(σ = p[:σ][i], ρ = p[:ρ][i])
   wca[i] = TPT.LWCA(ahs[i], p[:T][i], struct=:full)
-  # tb[i] = TPT.WHTB(p[:zd][i], p[:rd][i], version=:original)
+  tb[i] = TPT.WHTB(p[:zd][i], p[:rd][i], version=:modified)
 end
 
 a₀ = p[:a]
 a = zeros(N)
-rc₀ = p[:rc]
-rc = zeros(N)
 res = zeros(N)
 sys = Vector{TPT.TPTSystem}(N)
 F = Vector{Vector{Float64}}(N)
@@ -66,96 +62,84 @@ F = Vector{Vector{Float64}}(N)
 #
 # Performe optimization of rc
 #
-# Threads.@threads for i in 1:N
-for i in 5
-  function foptrc(rc::Float64)::Float64
-    @printf "rc = %1.5f\n" rc
+Threads.@threads for i in 1:N
+  function fopt(a::Float64)::Float64
+    pse = TPT.BretonnetSilbert(p[:zs][i], p[:rc][i], a)
+    nfe = TPT.NFE(wca[i], pse)
+    nfetb = TPT.NFETB(nfe, tb[i])
+    sys[i] = TPT.TPTSystem(wca[i], nfetb)
 
-    function fopta(a::Float64)::Float64
-      local ahs = TPT.AHS(σ = p[:σ][i], ρ = p[:ρ][i], approx=:RFA)
-      local wca = TPT.LWCA(ahs, p[:T][i], struct=:full)
-      local pse = TPT.BretonnetSilbert(p[:zs][i], rc, a)
-      local nfe = TPT.NFE(wca, pse)
-      local tb = TPT.WHTB(p[:zd][i], p[:rd][i], version=:original)
-      local nfetb = TPT.NFETB(nfe, tb)
-      sys[i] = TPT.TPTSystem(wca, nfetb)
-      local g = TPT.paircorrelation(sys[i])[1,1]
+    g = TPT.paircorrelation(sys[i])[1,1]
 
-      local residue::Float64 = 0
-      for j in 1:length(g_exp[i])
-        residue += abs(g_exp[i][j][2] - g(g_exp[i][j][1]))
-      end
-      residue
+    residue::Float64 = 0
+
+    for j in 1:length(g_exp[i])
+      residue += abs(g_exp[i][j][2] - g(g_exp[i][j][1]))
     end
 
-    opt = Optim.optimize(fopta, 0.7a₀[i], 1.3a₀[i], rel_tol = 1e-3)
-    a[i] = Optim.minimizer(opt)
-    @printf "a = %1.5f\n" a[i]
+    return residue
+  end
 
-    F[i] = Vector{Float64}(7)
-    Threads.@threads for k in 1:7
-      local ρ = (0.7 + (k-1)*0.1) * p[:ρ][i]
+  # fnlopt(x::Vector{Float64}, g)::Float64 = fopt(x[1])
+  #
+  # opt = NLopt.Opt(:GN_DIRECT, 1)
+  # NLopt.min_objective!(opt, fnlopt)
+  # NLopt.stopval!(opt, 1e-0)
+  # NLopt.xtol_abs!(opt, 1e-3)
+  # NLopt.ftol_abs!(opt, 1e-4)
+  # NLopt.lower_bounds!(opt, [0.5rc₀[i]])
+  # NLopt.upper_bounds!(opt, [1.5rc₀[i]])
+  #
+  # (fmin, xmin, result) = NLopt.optimize(opt, [rc₀[i]])
+  #
+  # res[i] = result
+  # rc[i] = xmin[1]
+  # fopt(xmin)
 
-      local ahs = TPT.AHS(σ = p[:σ][i], ρ = ρ, approx=:RFA)
-      local wca = TPT.LWCA(ahs, p[:T][i], struct=:full)
-      local pse = TPT.BretonnetSilbert(p[:zs][i], rc, a[i])
-      local nfe = TPT.NFE(wca, pse)
-      local tb = TPT.WHTB(p[:zd][i], p[:rc][i], version=:original)
-      local nfetb = TPT.NFETB(nfe, tb)
-      sys[i] = TPT.TPTSystem(wca, nfetb, m = p[:m][i])
+  result = Optim.optimize(fopt, 0.25, 0.35, rel_tol = 1e-3)
+  a[i] = Optim.minimizer(result)
+  res[i] = Optim.minimum(result)
 
-      F[i][k] = TPT.helmholtz(sys[i])
-    end
-    @show F[i]
+  fopt(a[i])
 
-    spl = Dierckx.Spline1D(collect(0.7:0.1:1.3), F[i], k=2, bc="error")
-    opt = Optim.optimize(x -> spl(x), 0.7, 1.3)
+  #
+  # Density dependency of free-energy
+  #
+  F[i] = Vector{Float64}(5)
+  for k in 1:5
+    ρₖ = (0.8 + (k-1)*0.1) * p[:ρ][i]
 
-    residue = abs(1.0 - Optim.minimizer(opt))
-    @show residue
-  end # foptrd
+    ahsₖ = TPT.AHS(σ = p[:σ][i], ρ = ρₖ)
+    wcaₖ = TPT.LWCA(ahsₖ, p[:T][i], struct=:full)
+    tbₖ = TPT.WHTB(p[:zd][i], p[:rd][i], version=:modified)
+    pseₖ = TPT.BretonnetSilbert(p[:zs][i], p[:rc][i], a[i])
+    nfeₖ = TPT.NFE(wcaₖ, pseₖ)
+    nfetbₖ = TPT.NFETB(nfeₖ, tbₖ)
+    sysₖ = TPT.TPTSystem(wcaₖ, nfetbₖ, m = p[:m][i])
 
-  fnlopt(x::Vector{Float64}, g)::Float64 = foptrc(x[1])
-
-  opt = NLopt.Opt(:GN_DIRECT, 1)
-  # opt = NLopt.Opt(:LN_BOBYQA, 1)
-  NLopt.min_objective!(opt, fnlopt)
-  NLopt.stopval!(opt, 1e-2)
-  NLopt.xtol_abs!(opt, 1e-3)
-  NLopt.ftol_abs!(opt, 1e-4)
-  NLopt.lower_bounds!(opt, [0.8rc₀[i]])
-  NLopt.upper_bounds!(opt, [1.2rc₀[i]])
-
-  (fmin, xmin, result) = NLopt.optimize(opt, [rc₀[i]])
-
-  res[i] = fmin
-  rc[i] = xmin[1]
-  foptrc(rc[i])
+    F[i][k] = TPT.helmholtz(sysₖ)
+  end
 end
+
 
 #
 # Print the numerical results to console
 #
 println("The optimized values of a:")
-for i in 5
-  @printf "  %2s: %1.5f\n" p[:X][i] a[i]
-end
-
-println("The optimized values of rc:")
-for i in 5
-  @printf "  %2s: %1.5f\n" p[:X][i] rc[i]
+for i in 1:N
+  @printf "  %2s: %1.3f\n" p[:X][i] a[i]
 end
 
 rmin = zeros(N)
 println("The positions of minimum of pairpotentials:")
-for i in 5
+for i in 1:N
   rmin[i] = sys[i].ref.rmin[1,1]
   @printf "  %2s: %1.3f\n" p[:X][i] rmin[i]
 end
 
 σ_hs = zeros(N)
 println("The effective hard-sphere diameters:")
-for i in 5
+for i in 1:N
   σ_hs[i] = sys[i].ref.trial.σ[1]
   @printf "  %2s: %1.3f\n" p[:X][i] σ_hs[i]
 end
@@ -164,19 +148,19 @@ end
 #
 # Save the numerical results as a csv file
 #
-df = DataFrame(X = p[:X], a = round(a, 5), rc = round(a, 5), res = round(res, 1), rmin = round(rmin, 3), σ_hs = round(σ_hs, 3))
+df = DataFrame(X = p[:X], a = round(a, 3), res = round(res, 1), rmin = round(rmin, 3), σ_hs = round(σ_hs, 3))
 writetable(joinpath(resdir, "results.csv"), df)
 
 # Overwrite the parameters for the subsequent tests
-p[:a] = round(a, 5)
-p[:rc] = round(a, 5)
+p[:a] = round(a, 3)
 writetable(joinpath("data", "parameters", "tm_optim.csv"), p)
 
 #
 # Save the graphical results as png files
 #
-for i in 7
-  # 1. Pairpotentials
+
+# 1. Pairpotentials
+for i in 1:N
   u_nfe = TPT.pairpotential(sys[i].pert.nfe)[1,1]
   u_tb = TPT.pairpotential(sys[i].pert.tb)[1,1]
   u_tot = TPT.pairpotential(sys[i].pert)[1,1]
@@ -184,17 +168,21 @@ for i in 7
   plot([u_nfe, u_tb, u_tot], 2, 20, ylims=(-0.05, 0.05), labels = ["NFE" "TB" "total"], xlabel="r (a.u.)", ylabel="u(r) (a.u.)")
   vline!([σ_hs[i]], label="HS")
   png(joinpath(resdir, "$(i)-$(p[:X][i])_u"))
+end
 
-  # 2. Pair correlation functions
+# 2. Pair correlation functions
+for i in 1:N
   g_hs = TPT.paircorrelation(ahs[i])[1,1]
   g_wca = TPT.paircorrelation(sys[i])[1,1]
 
   plot([g_hs, g_wca], 2, 20, labels=["HS" "WCA"], xlabel="r (a.u.)", ylabel="g(r)", ylims=:auto)
   plot!(g_exp[i], label="exp", xlims=(2,20), ylims=:auto)
   png(joinpath(resdir, "$(i)-$(p[:X][i])_g"))
+end
 
-  # 3. Density dependency of free-energy
-  plot(0.7:0.1:1.3, 2625.5*F[i], xlabel="Relative density", ylabel="F (kJ/mol)", ylims=:auto)
+# 3. Density dependency of free-energy
+for i in 1:N
+  plot(0.8:0.1:1.2, 2625.5*F[i], xlabel="Relative density", ylabel="F (kJ/mol)", ylims=:auto)
   png(joinpath(resdir, "$(i)-$(p[:X][i])_F"))
 end
 
